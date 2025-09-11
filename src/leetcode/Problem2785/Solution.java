debb45ce007939160b7b10664d43fc19ee0ffaed
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2025-09-11 21:22:21 +0300

    Added Solution to problem 2785(#5)

---
+++ src/leetcode/Problem2785/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public String sortVowels(String s) {
+        List<Character> vowels = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                vowels.add(c);
+            }
+        }
+        Collections.sort(vowels);
+        StringBuilder sb = new StringBuilder();
+        int vIndex = 0;
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                sb.append(vowels.get(vIndex++));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    private boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+}
